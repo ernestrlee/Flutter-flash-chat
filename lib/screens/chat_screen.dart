d9dd32266bfa297f1b11d360c4ffd1d805daaafd
Ernest-Lee <ernestrlee@yahoo.com>
2020-06-04 23:02:09 -0700

    Added chat styling.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -141,9 +141,32 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      '$text from $sender',
-      style: TextStyle(fontSize: 50.0),
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(sender,
+          style: TextStyle(
+            fontSize: 12.0,
+            color: Colors.black54,
+          )),
+          Material(
+            borderRadius: BorderRadius.circular(30.0),
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
