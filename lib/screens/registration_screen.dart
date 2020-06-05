74baa1f10397a287244db64b3900d2439d70fe89
Ernest-Lee <ernestrlee@yahoo.com>
2020-06-04 17:49:00 -0700

    Changed textfield style for email and password.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -10,6 +10,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +34,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
                 decoration: kTextFieldDecoration.copyWith(
                   hintText: 'Enter your email',
@@ -42,8 +47,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 hintText: 'Enter your password.',
@@ -58,7 +65,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 color: Colors.blueAccent,
                 buttonText: 'Register',
                 onPressed: () {
-                  Navigator.pushNamed(context, RegistrationScreen.id);
+                  print(email);
+                  print(password);
                 },
               ),
             ),
